6951f961b89ca95c5a6f3d102800b48d8be65629
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-06-02 18:49:04 +0300

    add cost form

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import CostActions from './Buttons/CostActions'
 import CostScale from './CostScale'
 import CostList from './CostList'
+import NewCost from './CostForm/NewCost'
 
 const MainComponent = () => {
     return (
         <>
+            <NewCost />
             <CostActions />
             <CostScale />
             <CostList />
