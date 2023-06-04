fe6223620e3df8bdb67143a4a6a1c59fec710f54
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-06-04 23:05:52 +0300

    add and display costs

--- src/components/Costs/CostItem.jsx
+++ src/components/Costs/CostItem.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React from "react";
 
-const CostItem = () => {
-    return (
-        <>
-            <p>Data</p>
-            <p>Title</p>
-            <p>Price</p>
-        </>
-    )
-}
+const CostItem = ({ date, title, price }) => {
+  const year = date.getFullYear;
+  const month = date.toLocaleString("ru-RU", { month: "long" });
+  const day = date.toLocaleString("ru-RU", { day: "2-digit" });
 
-export default CostItem
+  return (
+    <>
+      <div>
+        <div>
+          <div>{year}</div>
+          <div>{month}</div>
+          <div>{day}</div>
+        </div>
+        <p>{title}</p>
+        <p>{price}р.</p>
+      </div>
+    </>
+  );
+};
+
+export default CostItem;
