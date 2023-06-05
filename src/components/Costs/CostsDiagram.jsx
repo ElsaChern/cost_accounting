49471cc1ea51e5301ffb58412d395050cd72ba74
Elizabeth <elizaveta.chernosheikina@yandex.ru>
2023-06-05 15:44:06 +0300

    add spending chart and add some stylefor it

---
+++ src/components/Costs/CostsDiagram.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Diagram from "../Diagram/Diagram";
+
+const CostsDiagram = (props) => {
+  const diagramData = [
+    { label: "Янв", value: "0" },
+    { label: "Фев", value: "0" },
+    { label: "Мар", value: "0" },
+    { label: "Апр", value: "0" },
+    { label: "Май", value: "0" },
+    { label: "Июн", value: "0" },
+    { label: "Июл", value: "0" },
+    { label: "Авг", value: "0" },
+    { label: "Сен", value: "0" },
+    { label: "Окт", value: "0" },
+    { label: "Ноя", value: "0" },
+    { label: "Дек", value: "0" },
+  ];
+
+  for (let cost of props.costs) {
+    const costMonth = cost.date.getMonth();
+    diagramData[costMonth].value += cost.price;
+  }
+
+  return (
+    <div>
+      <Diagram dataSets={diagramData} />
+    </div>
+  );
+};
+
+export default CostsDiagram;
